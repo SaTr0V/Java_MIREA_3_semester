bd603d3c28c4ab956716c7559836467ea18a160b
SaTr0V <alexander1308@inbox.ru>
2025-09-03 09:32:32 +0300

    Решение задачи 1.2. Добавлено склонение валют.

--- src/pr_1/Main.java
+++ src/pr_1/Main.java
@@ -4,13 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-
         Scanner in = new Scanner(System.in);
         System.out.print("Yuan: ");
-        double yuan = in.nextInt();
+        int yuan = in.nextInt();
 
         double roubles = yuan * 11.91;
 
-        System.out.println(yuan + " Yuan = " + roubles + " Roubles");
+        if (yuan > 5 && yuan < 21) {
+            System.out.print(yuan + " Юайней" + " равняется ");
+        }
+        else if (yuan % 10 == 1) {
+            System.out.print(yuan + " Юань" + " равняется ");
+        }
+        else if (yuan % 10 > 1 && yuan % 10 < 5) {
+            System.out.print(yuan + " Юаня" + " равняется ");
+        }
+        else {
+            System.out.print(yuan + " Юаней" + " равняется ");
+        }
+
+        if ((int)roubles != roubles) {
+            System.out.print(roubles + " Рубля.");
+        }
+        else {
+            if (roubles % 10 == 1) {
+                System.out.print(roubles + " Рублю.");
+            }
+            else if (roubles >= 1000 && roubles % 10 == 0) {
+                System.out.print(roubles + " Рублей.");
+            }
+            else {
+                System.out.print(roubles + " Рублям.");
+            }
+        }
     }
 }
