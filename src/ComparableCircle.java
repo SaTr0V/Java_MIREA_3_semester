5c660dd1bffd65140aebc6bb6600629f0dd79554
SaTr0V <alexander1308@inbox.ru>
2025-09-23 09:40:55 +0300

    Решение задачи 4.5. Соответствие введенного числа месяцу

--- src/ComparableCircle.java
+++
@@ -1,8 +0,0 @@
-class ComparableCircle extends Circle implements Comparable<GeometricObject> {
-    public ComparableCircle(double radius) { super(radius); }
-
-    @Override
-    public int compareTo(GeometricObject other) {
-        return Double.compare(this.getArea(), other.getArea());
-    }
-}
--- src/main.java
+++ src/main.java
@@ -1,108 +1,31 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("TEST PROGRAM");
+        String[] months = {"январь", "февраль", "март", "апрель", "май",
+                            "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"};
+        int[] dom = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        Triangle triangle = null;  // инициализация объекта до конструкции if...else, иначе ошибка
-
-        System.out.println("Do you wish to create your own triangle or use a default one?\n" +
-                "Type 1 to create a triangle.\n" +
-                "Type 2 to use a default triangle.");
         Scanner in = new Scanner(System.in);
-        String cmd = in.nextLine();
 
-        if (cmd.equalsIgnoreCase("1")) {
-            System.out.print("Enter side 1 length: ");
-            double side1 = in.nextDouble();
+        try {
+            System.out.print("Введите целое число от 1 до 12: ");
+            int month_number = in.nextInt();
 
-            System.out.print("Enter side 2 length: ");
-            double side2 = in.nextDouble();
-
-            System.out.print("Enter side 3 length: ");
-            double side3 = in.nextDouble();
-
-            try {
-                triangle = new Triangle(side1, side2, side3);
-
-                in.nextLine();  // очистка буфера после nextDouble
+            // Проверка, входит ли введенное число в диапазон
+            if (month_number < 1 || month_number > 12) {
+                throw new ArrayIndexOutOfBoundsException();
             }
-            catch (IllegalTriangleException e) {
-                System.out.println("Error: " + e.getMessage());
-                System.exit(-1);
-            }
+
+            System.out.println("Месяц: " + months[month_number - 1]);
+            System.out.println("Количество дней: " + dom[month_number - 1]);
         }
-        else if (cmd.equalsIgnoreCase("no")) {
-            triangle = new Triangle();
+        catch (InputMismatchException e) {
+            System.out.println("Ошибка: введено не целое число.");
         }
-
-        System.out.println("The triangle is successfully created!");
-
-        System.out.println("\nDo you want to fill the triangle with a color? (yes/no)");
-        cmd = in.nextLine();
-
-        if (cmd.equalsIgnoreCase("yes")) {
-            triangle.setFilled(true);  // установка заполненности filled на true
-
-            System.out.print("Enter new color: ");
-            String color = in.nextLine();
-            triangle.setColor(color);
-        }
-
-        System.out.println("\n" + triangle.toString() + "\n");
-
-        // Круги
-        Circle circle_1 = new Circle(15);
-        circle_1.printCircle();
-        System.out.println();
-
-        Circle circle_2 = new Circle(5, "Green", true);
-        circle_2.printCircle();
-        System.out.println();
-
-        // Сравнение
-        GeometricObject.printMax(circle_1, circle_2);
-
-        System.out.println();
-
-        // Сравнение прямоугольников
-        Rectangle rectangle_1 = new Rectangle(2, 5);
-        System.out.println(rectangle_1.toString() + "\n");
-
-        Rectangle rectangle_2 = new Rectangle(10, 3);
-        System.out.println(rectangle_2.toString() + "\n");
-
-        GeometricObject.printMax(rectangle_1, rectangle_2);
-
-        // Сравнение "сравниваемых" кругов
-        ComparableCircle comp_1 = new ComparableCircle(2);
-        ComparableCircle comp_2 = new ComparableCircle(4);
-
-        int result1 = comp_1.compareTo(comp_2);
-        if (result1 > 0) {
-            System.out.println("Comparable circle 1 (area = " + comp_1.getArea() + ")" +
-                    " is bigger than comparable circle 2 (area = " + comp_2.getArea() + ")");
-        }
-        else if (result1 < 0) {
-            System.out.println("Comparable circle 2 (area = " + comp_2.getArea() + ")" +
-                    " is bigger than comparable circle 1 (area = " + comp_1.getArea() + ")");
-        }
-        else {
-            System.out.println("The circles are equally as big");
-        }
-
-        // Сравнение круга и прямоугольника
-        int result2 = comp_1.compareTo(rectangle_1);
-        if (result2 > 0) {
-            System.out.println("Comparable circle (area = " + comp_1.getArea() + ")" +
-                                " is bigger than the rectangle (area = " + rectangle_1.getArea() + ")");
-        }
-        else if (result2 < 0) {
-            System.out.println("The rectangle (area = " + rectangle_1.getArea() + ")" +
-                    " is bigger than the comparable circle (area = " + comp_1.getArea() + ")");
-        }
-        else {
-            System.out.println("The circle and the rectangle are equally as big");
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Недопустимое число. Введите число от 1 до 12.");
         }
     }
 }
